5f910025c0cbe0c136414b4f5618aea8e5ccfed8
Prakhar37 <106993856+Prakhar37@users.noreply.github.com>
2024-04-16 02:02:43 +0530

    Add files via upload

---
+++ Frontend/src/Pages/Home.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import HeroSection from '../componenets/HeroSection'
+import About from '../componenets/About'
+import Qualities from '../componenets/Qualities'
+import Menu from '../componenets/Menu'
+import WhoAreWe from '../componenets/WhoAreWe'
+import Team from '../componenets/Team'
+import Reservation from '../componenets/Reservation'
+import Footer from '../componenets/Footer'
+
+const Home = () => {
+  return (
+    <>
+       <HeroSection/>
+       <About/>
+       <Qualities/>
+       <Menu/>
+       <WhoAreWe/>
+       <Team/>
+       <Reservation/>
+       <Footer/>
+    </>
+  )
+}
+
+export default Home
---
+++ Frontend/src/componenets/Footer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left">SUBLIME</div>
+          <div className="right">
+            <p>J-07 Hirapur IIT (ISM) Dhanbad</p>
+            <p>Open: 10:00 AM - 10:00 PM</p>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="left">
+            <p>Developed By Prakhar Shrivastava</p>
+          </div>
+          <div className="right">
+            <p>All Rights Reserved By Prakhar Shrivastava.</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
